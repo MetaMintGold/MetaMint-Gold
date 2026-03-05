8e2dcdb6a27d3920775642b12d9b1cb5d62289ff
MetaMintGold <sherbazmustafa94@gmail.com>
2026-03-05 11:28:39 +0500

    Implement service worker with caching strategy

--- sw.js
+++ sw.js
@@ -1,3 +1,34 @@
-self.addEventListener('fetch', function(event) {
-  // Service worker is active
+const CACHE_NAME = 'metamint-cache-v2';
+const assets = [
+  '/',
+  'index.html',
+  'manifest.json',
+  'logo.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
